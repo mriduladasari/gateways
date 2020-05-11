0cc68765ebecd88b59fbb4bf3108c7097bd1d49f
Mridula Dasari <mriduladasari@gmail.com>
2020-05-10 22:56:26 -0500

    gates created

--- assets/js/gates.js
+++ assets/js/gates.js
@@ -1,22 +1,44 @@
-class GreenGate {
-  x = 60;
-  y = 500;
-  };
+class Gate {
+  x = 0;
+  y = 0;
+  type = 1;
 
-  imageSrc = 'assets/img/green-leftside-gate.png';
-  width = 24;
-  height = 124;
+  images = [
+    './assets/img/green-leftside-gate.png',
+    './assets/img/pink-leftside-gate.png',
+    './assets/img/purple-rightside-gate.png',
+  ];
 
-  img;
+  image;
+  imageSrc;
 
-  constructor() {
-    this.img = new Image();
-    this.img.src = this.imageSrc;
+  constructor(options = { type: 1, x: 0, y: 0 }) {
+    this.type = options.type - 1;
+    this.x = options.x || this.x;
+    this.y = options.y || this.y;
+
+    this.image = new Image();
+    this.image.onload = () => {
+      this._draw();
+    };
+    this.image.src = this.images[this.type];
   }
 
-  draw() {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  };
+  tick() {
+    this._draw();
+  }
 
+  _draw() {
+    ctx.drawImage(this.image, this.x, this.y);
+  }
 }
 
+// class Dog extends Animal {
+//   speak() {
+//     console.log(`${this.name} says, "Woof woof!"`);
+//   }
+
+//   fetch() {
+//     console.log('I got the toy! Throw it again!');
+//   }
+// }
